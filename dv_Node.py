9fc44209edc7c8a59adbf8170658d754c2d346cb
Brett <brp5088@rit.edu>
2021-05-04 22:07:51 -0400

    saving work; almostDoneMaking the Output

--- dv_Node.py
+++ dv_Node.py
@@ -1,9 +1,11 @@
+import sys
 
 class dv_Node():
 
     def __init__(self, name):
-        self.neighbors = []
+        self.nodesAwareOf = [ ]
         self.distanceTable = { }
+        self.routingTable = { }
         self.name = name
 
     def __str__( self ):
@@ -19,13 +21,56 @@ class dv_Node():
 
         for index, col_name in enumerate( col_keys ):
             if index == 0:
-                print( f"    { col_name }   ", end="")
+                print( f"     { col_name }    ", end="")
             else:
-                print( f" { col_name }   ", end="")
+                print( f" { col_name }    ", end="")
         print()
 
         for key in row_keys:
             string = " " + key + " "
             for col in col_keys:
-                string += " " + self.distanceTable[key][col] + "   "
+
+                if self.distanceTable[key][col] == "--":
+                    string += " " + self.distanceTable[key][col] + "   "
+                else:
+                    string += "  " + self.distanceTable[key][col] + "   "
             print( string )
+    
+
+    def print_routingTable( self ):
+
+        keys = self.nodesAwareOf
+        keys = sorted( keys )
+
+        print("Dest NxtHp")
+
+        for key in keys:
+            print( f" {key}    { self.routingTable[key] }" )
+
+
+
+    def generate_messages( self ):
+        string = ""
+
+        letter_lst = list(self.distanceTable.keys())
+        letter_lst = sorted( letter_lst )
+
+        for letter in letter_lst:
+            tmp = "{" + self.name + ","
+
+            min_letter = "?"
+            min_value = sys.maxsize
+            for sec_letter in sorted( self.nodesAwareOf ):
+
+                value = self.distanceTable[letter][sec_letter]
+                
+                if value != "--": 
+                    if min_value > int( value ):
+                        min_letter = sec_letter
+                        min_value = int( value )
+            
+            tmp += min_letter + "," + str( min_value ) + "}"
+            string += tmp
+
+        return string
+
--- dvrouting.py
+++ dvrouting.py
@@ -8,54 +8,73 @@ from dv_Node import *
 
 
 
-# def initialize_table( filepath, a_node):
+def initialize_distanceTable( init_line, a_node):
+    init_lst = init_line.split(",")
+
+    letters_arr = []
+    numbers_arr = []
+
+    for n in init_lst:
+        tmp = n.split("=")  # e.g., [ "B", "10" ]
+
+        letters_arr.append(tmp[0].strip())
+        numbers_arr.append(tmp[1].strip())
+
+    for index, letter in enumerate(letters_arr):
+
+        a_node.distanceTable[letter] = {}
+        for sec_letter in letters_arr:
+            if letter != sec_letter:
+                a_node.distanceTable[letter][sec_letter ] = "--"
+
+        a_node.distanceTable[letter][letter] = numbers_arr[index]
+        a_node.nodesAwareOf.append( letter )
+
+def initialize_routingTable( a_node ):
+    letters = a_node.nodesAwareOf
+    for letter in letters:
+        a_node.routingTable[letter] = letter
 
 
 
+
+
+def round_print( time, a_node ):
+    print(f"Time { time }:")
+    print("Distance Table for A:\n--------------------\nTo Via")
+    a_node.print_distanceTable()
+    print("\nRouting Table for A\n-------------------")
+    a_node.print_routingTable()
+    print( "\nMessages Sent:")
+    print( a_node.generate_messages() )
 
 
 def run( filepath ):
 
     a_node = dv_Node( "A" )
-
-
+    time = 0
 
     for index, line in enumerate( open( filepath, "r" ) ):
+
+        messages = ""
         
         if index > 0:
             break
 
-        print( f"index={index}" )
-        print( line )
+        # print( f"index={index}" )
+        # print( line )
 
         if index == 0:
-            init_lst = line.split( "," )
+            initialize_distanceTable( line, a_node )
+            initialize_routingTable( a_node )
+            round_print(time, a_node)
+        
+        if index > 0:
+            round_print( time, a_node )
+        
+        time += 1
+            
 
-            letters_arr = []
-            numbers_arr = []
-
-            for n in init_lst:
-                tmp = n.split( "=" ) # e.g., [ "B", "10" ]
-                
-                letters_arr.append( tmp[0].strip() )
-                numbers_arr.append( tmp[1].strip() )
-                
-            for index, letter in enumerate( letters_arr ):
-
-                a_node.distanceTable[letter] = {}
-
-                for sec_letter in letters_arr:
-
-                    if letter != sec_letter:
-
-
-                        # if a_node.distanceTable[letter] == None:
-                        #     a_node.distanceTable[letter] = {}
-                        a_node.distanceTable[ letter ][ sec_letter ] = "-"
-                
-                a_node.distanceTable[letter][letter] = numbers_arr[ index ]
-
-    a_node.print_distanceTable()
 
 
 
